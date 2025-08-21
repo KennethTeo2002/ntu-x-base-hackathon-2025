366c848a03d24ef9e6aa865c8052f759ffdbb54d
Edmund <koed.workspace@gmail.com>
2025-08-21 16:14:41 +0800

    Update App.js
    coded for prompt and prompt popout

--- storai/src/App.js
+++ storai/src/App.js
@@ -3,6 +3,8 @@ import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./theme";
 
 import Home from "./pages/Home"; // Import your Home component
+import Prompt from "./pages/Prompt"; // Import Prompt component
+import PromptPopOut from "./pages/PromptPopOut"; // Import PromptPopOut component
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/home" element={<Home />} />
+            <Route path="/prompt" element={<Prompt />} />
+            <Route path="/prompt-popout" element={<PromptPopOut />} />
           </Routes>
         </div>
       </Router>
