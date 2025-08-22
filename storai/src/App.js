4ae76cbba69ec22fa0f1ce8970c67e743317416b
Edmund <koed.workspace@gmail.com>
2025-08-22 11:10:38 +0800

    Add files via upload

--- storai/src/App.js
+++ storai/src/App.js
@@ -1,10 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./theme";
 
 import Home from "./pages/Home"; // Import your Home component
 import Prompt from "./pages/Prompt"; // Import Prompt component
 import PromptPopOut from "./pages/PromptPopOut"; // Import PromptPopOut component
+import Library from "./pages/Library"; // Import Library component
+import Story from "./pages/Story"; // Import Story component
 
 function App() {
   return (
@@ -15,7 +17,11 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/home" element={<Home />} />
             <Route path="/prompt" element={<Prompt />} />
+            <Route path="/prompt-result" element={<PromptPopOut />} />
             <Route path="/prompt-popout" element={<PromptPopOut />} />
+            <Route path="/library" element={<Library />} />
+            <Route path="/story/:storyId" element={<Story />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </div>
       </Router>
